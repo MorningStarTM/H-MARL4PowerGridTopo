0cca4308a9cfc39eb73552acb35ef39495a0b17f
ErnestBeckham <ernestbeckham1998@gmail.com>
2025-05-31 10:31:26 +0530

    updated

--- HMARL/config.py
+++ HMARL/config.py
@@ -1,1 +1,12 @@
-ENV_NAME = "l2rpn_case14_sandbox"
+iconfig = {"ENV_NAME" : "l2rpn_case14_sandbox",
+            "middle_agent_type" : "capa",  # Options: "capa", "fixed_sub"
+            "agent_type" : "ppo",  # Options: "ppo", "dqn", etc.
+            "input_dim" : 128,
+            'has_continuous_action_space': False,
+            'action_std_init': 0.6,
+            'lr_actor': 1e-4,
+            'lr_critic': 1e-4,
+            'gamma': 0.99,
+            'K_epochs': 80,
+            'eps_clip': 0.2,
+            }
